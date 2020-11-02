0584868228d55a371242c26ea2a872044ab42cd1
Akshat Surolia <45590045+AsianZeus@users.noreply.github.com>
2020-11-02 15:30:30 +0530

    app file updated

--- app.py
+++ app.py
@@ -18,28 +18,27 @@ def getContent(argv):
     data = json.loads(html)
     r = requests.get(data['items'][0]['link'])
     soup = BeautifulSoup(r.text, 'lxml')
-    content = soup.find('div',{'class':'entry-content'}).find('p').text
-    return content
+    content = soup.find('div',{'class':'entry-content'}).text
+    maxlines=5
+    cnt=0
+    resultx=''
+    for i in content.split('\n'):
+        if i=='':
+            continue
+        if(cnt<maxlines):
+            resultx=resultx+'\n'+i
+            cnt+=1
+    return resultx
 
 @app.route('/')
 def my_fun():
-	try:
-		tech = request.args['tech']
-		print('----------------------',tech)
-		result = getContent(tech)
-	except:
-		tech=''
-		result=''
-	return f'<html><body><h1>{tech}</h1><p>{result}</p></body></html>'
+    try:
+        tech = request.args['tech']
+        result = getContent(tech)
+    except:
+        tech=''
+        result=''
+    return f'<html><body><h1>{tech}</h1><p>{result}</p></body></html>'
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-
-
-
-
-
-
-
-
